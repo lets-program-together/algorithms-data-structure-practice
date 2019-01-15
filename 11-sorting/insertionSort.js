e599bec597c4263e13e159c60c29ec072ce2c00b
= <=>
2019-01-15 17:08:29 -0500

    adds udemy solution

--- 11-sorting/insertionSort.js
+++ 11-sorting/insertionSort.js
@@ -21,3 +21,21 @@ const insertionSort = arr => {
 
   return arr
 }
+
+// function insertionSort(arr) {
+//   const length = array.length;
+//   for (let i = 0; i < length; i++) {
+//     if (array[i] < array[0]) {
+//       // if current number is small than the first, move to first pos
+//       array.unshift(array.splice(i, 1)[0]);
+//     } else {
+//       // find where the number should go
+//       for (let j = 1; j < i; j++) {
+//         if (array[i] > array[j - 1] && array[i] < array[j]) {
+//           array.splice(j, 0, array.splice(i, 1)[0]);
+//         }
+//       }
+//     }
+
+//   }
+}
