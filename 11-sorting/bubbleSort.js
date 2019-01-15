52c5045b5cc27decc078f3796c4b99f490a1b859
= <=>
2019-01-15 14:46:47 -0500

    Implement bubble sort

---
+++ 11-sorting/bubbleSort.js
@@ -0,0 +1,19 @@
+const bubbleSort = arr=> {
+  if (arr.length <= 1) {
+    return arr
+  }
+  let swapped = true;
+  while (swapped) {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      let j = (i + 1);
+      const first = arr[i];
+      const second = arr[j];
+      if (first > second) {
+        [arr[i], arr[j]] = [second, first];
+        swapped = true;
+      }
+    }
+  }
+  return arr
+}
